bf5600c86ddcb5fa6b436f09b3843c1abbed2c69
infinitemax <m.brookman.byrne@gmail.com>
2023-10-31 11:12:16 +0000

    start to add backend functionality for register; bring this branch up to date with v1

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const createError = require("http-errors");
+
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "http://localhost:3001",
     })
 );
 app.use(morgan("dev"));
