4dbd85cd5eb6c42f18c0bce6b094c2950ac674b0
infinitemax <m.brookman.byrne@gmail.com>
2023-10-31 15:55:16 +0000

    resolve cors error and make register a client component

--- frontend/apiClient.js
+++ frontend/apiClient.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
-const url = process.env.NEXT_PUBLIC_API_ENDPOINT || 'http://localhost:3001/';
+const url = process.env.NEXT_PUBLIC_API_ENDPOINT || 'http://localhost:3001';
 
 export class ApiClient {
   constructor() {
    
   }
 
-  async register(email, password) {
+  async register(email, username, password) {
     try {
       const response = await axios.post(`${url}/register`, {
         email,
