e7507e48c899a28d1c928499bc261867edce3b1c
infinitemax <m.brookman.byrne@gmail.com>
2023-11-01 15:06:03 +0000

    add login function, using bcrypt and jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const { User } = require("../models/User");
 const createError = require("http-errors");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 // REGISTER A NEW USER
 exports.register = async (req, res, next) => {
@@ -38,7 +39,7 @@ exports.register = async (req, res, next) => {
     }
 
     try {
-        const hashed = await bcrypt.hash(password, saltRounds);
+        const hashed = await bcrypt.hash(password, saltRounds); // does this need a callback to deal with errors?
 
         const newUser = new User({
             username: username,
@@ -63,7 +64,51 @@ exports.register = async (req, res, next) => {
 
 // USER LOGIN
 exports.login = async (req, res, next) => {
-    res.json({message: "login route"})
+    const {username, password} = req.body
+
+    // find user
+    const user = await User.findOne({username: username})
+
+    if (!user) {
+        return res.status(404).json({
+            status: 404,
+            message: "user not found"
+        })
+    }
+
+    //check password
+    const hash = user.password
+    bcrypt.compare(password, hash, function(err, result) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        if (!result) {
+            return res.status(401).json({
+                status: 401,
+                message: "incorrect password",
+            })
+        }
+        // if a user logs in successfully, they are given a jwt via generateToken()
+        if (result) {
+            user.token = generateToken();
+            return res.status(200).json({
+                status: 200,
+                message: "user successfully logged in",
+                user
+            })
+        }
+    })
+
+    
+}
+
+// CREATE JWT
+// a function to create a jwt based on the user id, which is passed in and then used as the payload for the jwt method. As an option we have set the jwt to expire in 30 days
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: "30d"
+    })
 }
 
 // SERVE USER DATA
