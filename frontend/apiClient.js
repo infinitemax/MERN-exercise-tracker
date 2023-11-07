82686d1c5ca483f13e657fc8e8f4e9e01ee98780
infinitemax <m.brookman.byrne@gmail.com>
2023-11-07 16:14:29 +0000

    create basic bards to display activities i n the dashboard

--- frontend/apiClient.js
+++ frontend/apiClient.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const url = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3001";
 
+// this line adds the JWT to our cookies.
+// axios.defaults.withCredentials = true
+
 export class ApiClient {
   constructor() {}
 
@@ -12,13 +15,47 @@ export class ApiClient {
         password,
         username,
       });
-      return response;  // Return the response to the caller
+      return response; // Return the response to the caller
     } catch (error) {
-      console.error('Error in registration:', error);
+      console.error("Error in registration:", error);
       // Here we throw the error to let the caller handle it as per its own logic
       throw error;
     }
   }
+
+  // login call
+  async login(username, password) {
+    try {
+      console.log("this is the api client speaking!");
+      console.log(`credentials are: ${username} and ${password}`);
+      const response = await axios.post(
+        `${url}/login`,
+        {
+          username,
+          password,
+        },
+        { withCredentials: true } // note - this line allows the cookie to be updated.
+      );
+      console.log(response)
+      return response;
+    } catch (error) {
+      // note from MBB - using error.response here, which I hear is a good way to make use of Axios' error object.
+      return error.response;
+
+    }
+  }
+
+    async loadUserData() {
+    try {
+
+      const response = await axios.get(`${url}/myarea`,
+      {withCredentials: true})
+      return response;
+    }catch (error) {
+      console.log(error)
+      return error
+    }
+  } 
 }
 
 export default new ApiClient();
---
+++ frontend/app/myarea/page.js
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import Dashboard from "@/components/Dashboard";
+import apiClient from "@/apiClient";
+import { useState, useEffect } from "react";
+
+export default function MyAreaPage() {
+    const [data, setData] = useState();
+    const [isAuthorised, setIsAuthorised] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect( () => {
+        
+        getUserData();
+        console.log("how many times?")
+    }, []);
+
+
+
+    // load up the user's data
+    const getUserData = async () => {
+        try {
+            const response = await apiClient.loadUserData();
+
+            // if we don't get back 200, the user is not auth'd - set state and send message
+            if (response.status !== 200) {
+                setIsAuthorised(false);
+            }
+
+            // add user's activities to the activities variable
+            setData(response.data.user.activities)
+
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div>
+            {isLoading && <h2>Loading...</h2>}
+            {!isLoading && (
+                <>
+                    {!isAuthorised && (
+                        <h2>
+                            401 error - you are not authorised.{" "}
+                            <a className="text-blue-700" href="/login">
+                                Please login
+                            </a>
+                            .
+                        </h2>
+                    )}
+                    {isAuthorised && <Dashboard 
+                        data={data}
+                        hello="hello"
+                    />}
+                </>
+            )}
+        </div>
+    );
+}
