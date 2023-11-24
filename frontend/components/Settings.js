2788132bd5c6762f13f6310bf812d8e8d9651cf7
infinitemax <m.brookman.byrne@gmail.com>
2023-11-24 10:39:50 +0000

    final merge on this branch

--- frontend/components/Settings.js
+++ frontend/components/Settings.js
@@ -17,24 +17,22 @@ const Settings = (props) => {
     }
 
     return (
-        <div className="relative flex justify-center">
+        <div className="relative">
             {isUpdating && <UpdateSettings 
                 userInfo={userInfo}
                 closeModal={() => {closeModal()}}
                 handleDetailsUpdate={() => props.handleDetailsUpdate()}
             />}
-            <div className="py-8 px-4 bg-slate-300 mt-24 rounded-lg">
-                <h1 className="text-2xl text-center pb-4">{userInfo.username}'s settings</h1>
-                <p className="pb-2">View and update your details below</p>
-                <p>Username: {userInfo.username}</p>
-                <p>Email: {userInfo.email}</p>
-                <p>Name: {userInfo.firstName} {userInfo.lastName}</p>
-                <p>Date of birth: {userInfo?.dateOfBirth}</p>
-                <p>Height: {userInfo?.height} {userInfo?.height && "cm"}</p>
-                <p>Weight: {userInfo?.weight} {userInfo?.weight && "kg"}</p>
-                <p>Avatar: {userInfo?.avatar}</p>
-                <button className="py-2 px-4 bg-slate-600 hover:bg-slate-700 text-slate-200 rounded-full" onClick={() => {setIsUpdating(true)}}>Update details</button>
-            </div>
+            <h1>{userInfo.username}'s settings</h1>
+            <p>View and update your settings below</p>
+            <p>Username: {userInfo.username}</p>
+            <p>Email: {userInfo.email}</p>
+            <p>Name: {userInfo.firstName} {userInfo.lastName}</p>
+            <p>Date of birth: {userInfo?.dateOfBirth}</p>
+            <p>Height: {userInfo?.height} {userInfo?.height && "cm"}</p>
+            <p>Weight: {userInfo?.weight} {userInfo?.weight && "kg"}</p>
+            <p>Avatar: {userInfo?.avatar}</p>
+            <button className="py-2 px-4 bg-slate-600 hover:bg-slate-700 text-slate-200 rounded-full" onClick={() => {setIsUpdating(true)}}>Update details</button>
         </div>
     );
 };
