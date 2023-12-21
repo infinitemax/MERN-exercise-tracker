101d29b5999806dfb26f5a07f7c530ad24f78fbe
infinitemax <m.brookman.byrne@gmail.com>
2023-12-21 15:42:49 +0000

    add localhost back in to api url

--- frontend/goalsApiClinet.js
+++ frontend/goalsApiClinet.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.NEXT_PUBLIC_API_ENDPOINT;
+const url = process.env.NEXT_PUBLIC_API_ENDPOINT || "http://localhost:3001";
 
 // cut from the url line, add back for local use: || "http://localhost:3001";
 
